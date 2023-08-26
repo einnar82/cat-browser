b069b9adf4951ebb7cb2be1a77696e71c9cd7d2a
Rannie Ollit <einnar82@gmail.com>
2023-08-26 23:19:56 +0800

    [Chore] Update the build script

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: {
+        compilerOptions: {
+          compatConfig: {
+            MODE: 2
+          }
+        }
+      }
+    })
   ],
   resolve: {
     alias: {
